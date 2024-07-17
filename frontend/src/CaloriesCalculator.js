65eaace5752b2dbbda0427bcfa2255bc5d4f7792
stoyanivanov <stoyanivanov03@gmail.com>
2024-07-17 15:14:04 +0300

    frontend for adding new product

--- frontend/src/CaloriesCalculator.js
+++ frontend/src/CaloriesCalculator.js
@@ -2,13 +2,21 @@ import React, { useState, useEffect } from 'react';
 import axios from './axiosConfig';
 import styles from './CaloriesCalculator.module.css';
 
-
 const CaloriesCalculator = () => {
   const [date, setDate] = useState(new Date().toLocaleDateString());
   const [meals, setMeals] = useState([]);
   const [totals, setTotals] = useState({ calories: 0, proteins: 0, carbs: 0, fats: 0 });
   const [newMeal, setNewMeal] = useState({ productId: '', grams: '' });
   const [loading, setLoading] = useState(true);
+  const [showProductForm, setShowProductForm] = useState(false);
+  const [newProduct, setNewProduct] = useState({
+    name: '',
+    productType: '',
+    caloriesPer100Grams: '',
+    proteinPer100Grams: '',
+    fatPer100Grams: '',
+    carbsPer100Grams: '',
+  });
   const userId = 48; // Replace with the actual user ID
 
   useEffect(() => {
@@ -16,25 +24,24 @@ const CaloriesCalculator = () => {
     fetchTotals();
   }, [date]);
 
-
   const handleUnauthorized = (error) => {
     if (error.response && error.response.status === 401) {
-     window.location.href = '/login'; // Redirect to login page
+      window.location.href = '/login'; // Redirect to login page
     } else {
       console.error('Error:', error);
-       setLoading(false);
+      setLoading(false);
     }
   };
 
-const fetchMeals = async () => {
-  try {
-    const response = await axios.get(`meals/date/${userId}`, { params: { date } });
-    console.log('Meals fetched:', response.data); // Add this line to check the response
-    setMeals(response.data);
-  } catch (error) {
-    handleUnauthorized(error);
-  }
-};
+  const fetchMeals = async () => {
+    try {
+      const response = await axios.get(`meals/date/${userId}`, { params: { date } });
+      console.log('Meals fetched:', response.data); // Add this line to check the response
+      setMeals(response.data);
+    } catch (error) {
+      handleUnauthorized(error);
+    }
+  };
 
   const fetchTotals = async () => {
     try {
@@ -46,7 +53,7 @@ const fetchMeals = async () => {
         carbs: responseData.carb,
         fats: responseData.fat,
       });
-       setLoading(false);
+      setLoading(false);
     } catch (error) {
       handleUnauthorized(error);
     }
@@ -71,9 +78,33 @@ const fetchMeals = async () => {
     setNewMeal({ ...newMeal, [name]: value });
   };
 
-   if (loading) {
-      return <div className={styles.loader}>Loading...</div>;
+  const handleProductInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewProduct({ ...newProduct, [name]: value });
+  };
+
+  const handleAddProduct = async () => {
+    try {
+      console.log('Adding product:', newProduct);
+      const response = await axios.post('/new/product', newProduct);
+      console.log('Product added successfully:', response.data);
+      setNewProduct({
+        name: '',
+        productType: '',
+        caloriesPer100Grams: '',
+        proteinPer100Grams: '',
+        fatPer100Grams: '',
+        carbsPer100Grams: '',
+      });
+      setShowProductForm(false); // Hide the product form
+    } catch (error) {
+      console.error('Error adding product:', error);
     }
+  };
+
+  if (loading) {
+    return <div className={styles.loader}>Loading...</div>;
+  }
 
   return (
     <div className={styles.container}>
@@ -82,7 +113,7 @@ const fetchMeals = async () => {
       <MealList meals={meals} />
       <Totals totals={totals} />
       <div className={styles.addMealForm}>
-        <h2>Add Food</h2>
+        <h2>Add New Meal</h2>
         <input
           type="number"
           name="productId"
@@ -99,8 +130,78 @@ const fetchMeals = async () => {
           placeholder="Grams"
           required
         />
-        <button onClick={handleAddFood}>Add Food</button>
+        <button className={styles.addButton} onClick={handleAddFood}>Add Meal</button>
       </div>
+      <button className={`${styles.addButton} ${styles.addProductButton}`} onClick={() => setShowProductForm(!showProductForm)}>
+        {showProductForm ? 'Cancel' : 'Add New Product'}
+      </button>
+      {showProductForm && (
+        <div className={styles.addProductForm}>
+          <h2>Add New Product</h2>
+          <input
+            type="text"
+            name="name"
+            value={newProduct.name}
+            onChange={handleProductInputChange}
+            placeholder="Product Name"
+            required
+          />
+          <select
+           name="productType"
+           value={newProduct.productType}
+           onChange={handleProductInputChange}
+           placeholder="Product Type"
+           required
+            >
+           <option value="" disabled>Select Product Type</option>
+           <option value="MEAT">Meat</option>
+           <option value="FRUITS">Fruits</option>
+           <option value="VEGETABLES">Vegetables</option>
+           <option value="DAIRY">Dairy</option>
+           <option value="LEGUMES">Legumes</option>
+           <option value="CEREALS">Cereals</option>
+           <option value="TUBERS">Tubers</option>
+           </select>
+
+          <input
+            type="number"
+            name="caloriesPer100Grams"
+            value={newProduct.caloriesPer100Grams}
+            onChange={handleProductInputChange}
+            placeholder="Calories per 100 Grams"
+            min="1"
+            required
+          />
+          <input
+            type="number"
+            name="proteinPer100Grams"
+            value={newProduct.proteinPer100Grams}
+            onChange={handleProductInputChange}
+            placeholder="Protein per 100 Grams"
+            min="1"
+            required
+          />
+          <input
+            type="number"
+            name="fatPer100Grams"
+            value={newProduct.fatPer100Grams}
+            onChange={handleProductInputChange}
+            placeholder="Fat per 100 Grams"
+            min="1"
+            required
+          />
+          <input
+            type="number"
+            name="carbsPer100Grams"
+            value={newProduct.carbsPer100Grams}
+            onChange={handleProductInputChange}
+            placeholder="Carbs per 100 Grams"
+            min="1"
+            required
+          />
+          <button className={styles.addButton} onClick={handleAddProduct}>Add Product</button>
+        </div>
+      )}
     </div>
   );
 };
@@ -111,7 +212,7 @@ const MealList = ({ meals }) => {
   return (
     <div className={styles.foodList}>
       <h2>Food Eaten Today</h2>
-       <ol>
+      <ol>
         {meals.map((meal) => {
           console.log('Meal:', meal); // Log each meal
           const totalKcal = (meal.quantity / 100) * meal.product.caloriesPer100Grams;
@@ -123,7 +224,6 @@ const MealList = ({ meals }) => {
           );
         })}
       </ol>
-
     </div>
   );
 };
